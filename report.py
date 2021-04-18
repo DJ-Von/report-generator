bced4a40ff4139328732422a466f412bcc17e3b3
glebik <kotruntsev.gleb@yandex.ru>
2021-04-18 12:01:30 +0300

    Add files via upload

--- report.py
+++ report.py
@@ -1,6 +1,7 @@
 from docxtpl import DocxTemplate
 import yaml
 import sys
+from docx import Document
 
 doc = DocxTemplate("templ.docx")
 
@@ -12,8 +13,11 @@ task = conf.get('task')
 autor_name = conf.get('autor_name')
 group = conf.get('group')
 var_num = conf.get('var_num')
+img = conf.get('img')
 
 context = {'autor_name': autor_name, 'group': group, 'var_num': var_num, 'i' : i, 'work_name' : work_name, 'task': task}
+
+doc.replace_pic('car.jpg','ok.jpg')
 
 doc.render(context)
 doc.save("templ-final.docx")
