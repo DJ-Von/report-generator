3254ac14c1da5cc15b25f1e5b845e3006d3d788a
glebik <kotruntsev.gleb@yandex.ru>
2021-05-01 23:25:57 +0300

    Add files via upload

---
+++ add.pas
@@ -0,0 +1,14 @@
+Program main;
+Uses CRT;
+
+Var
+    a: Integer;
+    b: Integer;
+
+Begin
+    Write('Введите а');
+    Readln(a);
+    Write('Ввeдите b');
+    Readln(b);
+    Writeln(a + b);
+End.
